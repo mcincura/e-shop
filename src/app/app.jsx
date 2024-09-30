f62a38235cbde8fa2ea3bac2ed19d43fdb02954c
mcincura <mar.cincura@gmail.com>
2024-09-30 17:00:54 +0200

    Initial commit

---
+++ src/app/app.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+    return(
+        <div className='main-content'>
+            <h1>TEST</h1>
+        </div>
+    )
+};
+
+export default App
